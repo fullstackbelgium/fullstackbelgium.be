0c123ad9f8cce95e22bf84b688528b3abe88af68
Dries Vints <dries.vints@gmail.com>
2019-01-20 21:29:24 +0100

    Update favicons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,6 +10,14 @@
 
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
 
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/images/favicons/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/images/favicons/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#00aba9">
+    <meta name="theme-color" content="#ffffff">
+
     <style type="text/css">
         * {
             border: 0;
