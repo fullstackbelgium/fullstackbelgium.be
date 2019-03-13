cf629a49fa342827582a0145f90cf7e03e2414f9
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2019-03-13 15:03:33 +0100

    Dummy policy

--- resources/views/policy.blade.php
+++ resources/views/policy.blade.php
@@ -2,6 +2,9 @@
     'title' => 'Policy',
 ])
     <div class="wrapper">
-        Policy
+        <div class="markup md:w-3/4 pb-4 md:pb-8">
+            <h1>Policy</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit modi ea atque, cumque aliquam, laudantium doloremque ad autem at aspernatur in harum! Corrupti, quisquam voluptatem quasi quam veritatis repudiandae laborum.</p>
+        </div>
     </div>
 @endcomponent
