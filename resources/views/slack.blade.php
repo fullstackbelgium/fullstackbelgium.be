af203c7053c72c8dbf684056dccbef4d4f5b3d14
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2019-03-18 16:38:48 +0100

    Slack

--- resources/views/slack.blade.php
+++ resources/views/slack.blade.php
@@ -9,13 +9,15 @@
                 <p class="text-xl">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus illo est rem sit, perferendis, consequatur provident amet pariatur natus totam alias earum odio? Dicta quidem maxime minima sed laborum accusantium.</p>
             </div>
             <p>
-                <a class="button mb-1"
-                    target="_blank"
-                    href="https://join.slack.com/t/fullstackbelgium/shared_invite/enQtMzYxNjMwNTM3NDU2LTE5Nzk3NDkwMmNjMThhODM2MzM2MTE0NzMxZjIwNWZkNjAwMDc3NmZlYWQ0NGVmZTc4NWJkMzMzODAzZjVmZGU">
-                    Join us on Slack →
-                </a>
-                <span class="block text-2xs text-gray-600 font-normal ml-3">
-                    several Belgians are typing…
+                <span class="inline-flex flex-col items-end">
+                    <a class="button mb-1"
+                        target="_blank"
+                        href="https://join.slack.com/t/fullstackbelgium/shared_invite/enQtMzYxNjMwNTM3NDU2LTE5Nzk3NDkwMmNjMThhODM2MzM2MTE0NzMxZjIwNWZkNjAwMDc3NmZlYWQ0NGVmZTc4NWJkMzMzODAzZjVmZGU">
+                        Join us on Slack →
+                    </a>
+                    <span class="block text-2xs text-gray-600 font-normal">
+                        several Belgians are typing…
+                    </span>
                 </span>
             </p>
         </section>
