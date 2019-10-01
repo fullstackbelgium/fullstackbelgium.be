8024d1412f2709d3074ca52e6aae573d363c90b1
Dries Vints <dries.vints@gmail.com>
2019-10-01 14:44:02 +0200

    Update slack link

--- resources/views/slack.blade.php
+++ resources/views/slack.blade.php
@@ -14,7 +14,7 @@
                 <span class="inline-flex flex-col items-center">
                     <a class="button text-lg mb-1"
                         target="_blank"
-                        href="https://join.slack.com/t/fullstackbelgium/shared_invite/enQtMzYxNjMwNTM3NDU2LTE5Nzk3NDkwMmNjMThhODM2MzM2MTE0NzMxZjIwNWZkNjAwMDc3NmZlYWQ0NGVmZTc4NWJkMzMzODAzZjVmZGU">
+                        href="https://join.slack.com/t/fullstackbelgium/shared_invite/enQtMzYxNjMwNTM3NDU2LTJiN2NjZWI4NjRiN2Y5NmQxZDYxOWI0ZjRiYzk1NGM4MzI2Y2Q3ODNkZDhkMzdjMzQ5Mzg0MzQ4ZGI1ODMwODE">
                         Join us on Slack →
                     </a>
                     <span class="block text-2xs text-gray-600 font-normal">
