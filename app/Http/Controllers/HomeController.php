9e33730d51af04bda9eb99bafb8f1f9b9526c641
Rias <hello@rias.be>
2019-10-21 11:07:33 +0200

    wip

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,23 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Event;
-use App\Models\Meetup;
+use App\Http\ViewModels\HomepageViewModel;
 use App\Services\Meetup\MeetupApi;
 
 class HomeController
 {
     public function __invoke(MeetupApi $meetupApi)
     {
-        $meetups = Meetup::with('upcomingEvents', 'previousEvents')->get()->sortBy(function (Meetup $meetup) {
-            return optional($meetup->upcomingEvents->first())->date;
-        });
+        $homepageViewModel = new HomepageViewModel($meetupApi);
 
-        $totalAttendees = 0;
-        foreach ($meetups as $meetup) {
-            $totalAttendees += $meetupApi->getAttendees($meetup->previousEvents->last());
-        }
-
-        return view('home', compact('meetups', 'totalAttendees'));
+        return view('home', $homepageViewModel);
     }
 }
---
+++ app/Http/ViewModels/HomepageViewModel.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\ViewModels;
+
+use App\Models\Event;
+use App\Models\Meetup;
+use App\Models\Sponsor;
+use App\Services\Meetup\MeetupApi;
+use Spatie\SchemaOrg\Schema;
+use Spatie\ViewModels\ViewModel;
+
+class HomepageViewModel extends ViewModel
+{
+    /** @var \Illuminate\Database\Eloquent\Collection */
+    private $meetups;
+
+    /** @var \App\Services\Meetup\MeetupApi */
+    private $meetupApi;
+
+    public function __construct(MeetupApi $meetupApi)
+    {
+        $this->meetups = Meetup::with('upcomingEvents', 'previousEvents')->get()->sortBy(function (Meetup $meetup) {
+            return optional($meetup->upcomingEvents->first())->date;
+        });
+        $this->meetupApi = $meetupApi;
+    }
+
+    public function meetups()
+    {
+        return $this->meetups;
+    }
+
+    public function totalAttendees(): int
+    {
+        $totalAttendees = 0;
+        foreach ($this->meetups as $meetup) {
+            $totalAttendees += $this->meetupApi->getAttendees($meetup->previousEvents->last());
+        }
+
+        return $totalAttendees;
+    }
+
+    public function schemaOrg(): string
+    {
+        return $this->meetups->flatMap(function (Meetup $meetup) {
+            return $meetup->upcomingEvents->map(function (Event $event) use ($meetup) {
+                /** @var \Illuminate\Support\Carbon $startDate */
+                $startDate = $event->date;
+                $startDate->setHour(19)->setMinutes(0)->setSeconds(0);
+                $endDate = $startDate->copy()->setHour(22);
+
+                $performers = [];
+                if ($event->speaker_1_name) {
+                    $performers[] = Schema::person()
+                        ->name($event->speaker_1_name)
+                        ->url("https://twitter.com/{$event->speaker_1_twitter}")
+                        ->description($event->speaker_1_bio);
+                }
+
+                if ($event->speaker_2_name) {
+                    $performers[] = Schema::person()
+                        ->name($event->speaker_2_name)
+                        ->url("https://twitter.com/{$event->speaker_2_twitter}")
+                        ->description($event->speaker_2_bio);
+                }
+
+                /** @var \App\Models\Event $event */
+                $socialEvent = Schema::socialEvent()
+                    ->name($meetup->name)
+                    ->description($event->intro)
+                    ->performers($performers)
+                    ->startDate($startDate)
+                    ->endDate($endDate)
+                    ->url($event->meetup_com_url)
+                    ->image(Schema::imageObject()->url(asset('/storage/'.$meetup->logo)))
+                    ->location(
+                        Schema::place()
+                            ->name($event->venue_name)
+                            ->image(Schema::imageObject()->url(asset('/storage/'.$event->venue_logo)))
+                            ->address(
+                                Schema::postalAddress()
+                                    ->addressCountry('Belgium')
+                                    ->addressLocality(str_replace('Full Stack ', '', $meetup->name))
+                            )
+                    )
+                    ->doorTime($startDate)
+                    ->isAccessibleForFree(true)
+                    ->organizer([
+                        Schema::person()->name('Dries Vints')->url('https://driesvints.com'),
+                        Schema::person()->name('Rias Van der Veken')->url('https://rias.be'),
+                    ]);
+
+                $sponsors = $event->sponsors->map(function (Sponsor $sponsor) {
+                    return Schema::organization()
+                        ->name($sponsor->name)
+                        ->image(Schema::imageObject()->url(asset('/storage/'.$sponsor->logo)));
+                });
+
+                if (count($sponsors) > 0) {
+                    $socialEvent->sponsor($sponsors);
+                }
+
+                return $socialEvent->toScript();
+            });
+        })->join('');
+    }
+}
