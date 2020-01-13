f073699ff4397e58c13393ebc21819f99f50091d
Rias <hello@rias.be>
2020-01-13 19:07:59 +0100

    wip

--- resources/views/cfp.blade.php
+++ resources/views/cfp.blade.php
@@ -3,7 +3,25 @@
 ])
     <div class="wrapper">
         <div class="markup pb-4 md:pb-8">
-            <div class="typeform-widget" data-url="https://rias580405.typeform.com/to/DumEtr" data-transparency="100" data-hide-headers=true data-hide-footer=true style="width: 100%; height: 300px;"></div> <script> (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/"; if(!gi.call(d,id)) { js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })() </script> <div style="font-family: Sans-Serif;font-size: 12px;color: #999;opacity: 0.5; padding-top: 5px;"> powered by <a href="https://admin.typeform.com/signup?utm_campaign=DumEtr&utm_source=typeform.com-15250183-Free&utm_medium=typeform&utm_content=typeform-embedded-poweredbytypeform&utm_term=EN" style="color: #999" target="_blank">Typeform</a> </div>
+            <p class="text-xl">
+                Full Stack Belgium Lightning Talks CFP
+            </p>
+            <p class="text-gray-700">
+                For our March events in Ghent and Antwerp we're going to do two evenings of lightning talks!
+            </p>
+            <p class="text-gray-700">
+                We've opened up a CFP to gather talk ideas. Do you want to give a lightning talk? Submit your idea here.
+            </p>
+            <p class="text-gray-700 mb-1">
+                Ghent (2020-03-11): <a href="https://www.meetup.com/fullstackghent/events/rsgfgrybcfbpb/" target="_blank" rel="noopener">https://www.meetup.com/fullstackghent/events/rsgfgrybcfbpb/</a><br>
+            </p>
+            <p class="text-gray-700">
+                Antwerp (2020-03-25): <a href="https://www.meetup.com/fullstackantwerp/events/nkjsbrybcfbhc/" target="_blank" rel="noopener">https://www.meetup.com/fullstackantwerp/events/nkjsbrybcfbhc/</a>
+            </p>
+            <p class="text-gray-700">
+                CFP Deadline is Friday 21st of February.
+            </p>
+            <div class="typeform-widget" data-url="https://rias580405.typeform.com/to/DumEtr" data-transparency="100" data-hide-headers=true data-hide-footer=true style="width: 100%; height: 600px;"></div> <script> (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/"; if(!gi.call(d,id)) { js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })() </script> <div style="font-family: Sans-Serif;font-size: 12px;color: #999;opacity: 0.5; padding-top: 5px;"> powered by <a href="https://admin.typeform.com/signup?utm_campaign=DumEtr&utm_source=typeform.com-15250183-Free&utm_medium=typeform&utm_content=typeform-embedded-poweredbytypeform&utm_term=EN" style="color: #999" target="_blank">Typeform</a> </div>
         </div>
     </div>
 @endcomponent
