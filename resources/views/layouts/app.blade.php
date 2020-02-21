a2687db3f2563bd47436542e4627d94402a325e0
Rias <hello@rias.be>
2020-02-21 17:00:35 +0100

    🚧

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,8 +46,8 @@
     <div class="flex flex-col min-h-screen">
         <section class="bg-europe-dark border-t-4 border-europe-light text-white font-medium pt-3 pb-4 mb-4">
             <div class="wrapper text-center">
-                <a class="focus:underline focus:bg-transparent" href="/cfp">
-                    We’re doing lightning talks in March! Enter the CFP here →
+                <a class="focus:underline focus:bg-transparent" href="https://fullstackeurope.com/" target="_blank" rel="noopener">
+                    Join us at Full Stack Europe, our conference in Antwerp  →
                 </a>
             </div>
         </section>
