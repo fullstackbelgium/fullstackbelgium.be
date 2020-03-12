d4d03a544962877fa362e6c512331f98ded467d0
Dries Vints <dries.vints@gmail.com>
2020-03-12 20:38:06 +0100

    Try github

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -54,7 +54,7 @@ cd {{ $releasesDir }};
 mkdir {{ $newReleaseDir }};
 
 # Clone the repo
-git clone --depth 1 git@fsbe:{{ $repository }} {{ $newReleaseName }}
+git clone --depth 1 git@github.com:{{ $repository }} {{ $newReleaseName }}
 
 # Configure sparse checkout
 cd {{ $newReleaseDir }}
