4a6359000a9a38b63ec9843fcd7913043a098ab7
Rias <hey@rias.be>
2021-10-13 16:31:45 +0200

    Catch exception on Meetup API

--- app/Services/Meetup/MeetupServiceProvider.php
+++ app/Services/Meetup/MeetupServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Services\Meetup;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
 use Illuminate\Support\ServiceProvider;
 
 class MeetupServiceProvider extends ServiceProvider
@@ -10,18 +11,24 @@ class MeetupServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind(MeetupApi::class, function () {
-            $client = new Client();
-            $response = $client->post('https://secure.meetup.com/oauth2/access', [
-                'form_params' => [
-                    'client_id' => config('services.meetup.key'),
-                    'client_secret' => config('services.meetup.secret'),
-                    'grant_type' => 'refresh_token',
-                    'refresh_token' => config('services.meetup.refresh_token'),
-                ],
-            ]);
+            $token = '';
 
-            $jsonResponse = json_decode((string) $response->getBody()->getContents(), true);
-            $token = $jsonResponse['access_token'];
+            try {
+                $client = new Client();
+                $response = $client->post('https://secure.meetup.com/oauth2/access', [
+                    'form_params' => [
+                        'client_id' => config('services.meetup.key'),
+                        'client_secret' => config('services.meetup.secret'),
+                        'grant_type' => 'refresh_token',
+                        'refresh_token' => config('services.meetup.refresh_token'),
+                    ],
+                ]);
+
+                $jsonResponse = json_decode((string) $response->getBody()->getContents(), true);
+                $token = $jsonResponse['access_token'];
+            } catch (ClientException) {
+                // Do nothing
+            }
 
             $client = new Client([
                 'base_uri' => 'https://api.meetup.com/',
