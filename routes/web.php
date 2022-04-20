b5dc93f8bb267080e1fea7fb9f4f454bbfeac6a2
Dries Vints <dries@vints.io>
2022-04-20 11:17:44 +0200

    wip

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,7 @@ Route::group(['middleware' => ['cacheResponse:3600']], function () {
     Route::get('/', HomeController::class);
 });
 
-Route::view('meetups', 'meetups');
 Route::view('slack', 'slack');
 Route::view('contact', 'contact');
-Route::view('policy', 'policy');
 Route::view('code-of-conduct', 'code-of-conduct');
 Route::view('cfp', 'cfp');
